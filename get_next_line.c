be25986a2ff0fe932883f6f79db2db00816e25e1
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2024-11-13 10:06:09 +0100

    1337 bg

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: souaammo <souaammo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 19:12:06 by souaammo          #+#    #+#             */
-/*   Updated: 2024/11/12 19:48:22 by souaammo         ###   ########.fr       */
+/*   Updated: 2024/11/13 10:04:10 by souaammo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,24 @@ size_t	ft_strlen(const char *str)
 	return (l);
 }
 
-static void	*ft_free(char *temp)
-{
-	free(temp);
-	return (NULL);
-}
-
 static char	*ft_read_size(int fd, char *line)
 {
-	char	*temp;
+	char	buffer[BUFFER_SIZE + 1];
 	ssize_t	read_byte;
 
-	temp = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!temp)
-		return (NULL);
 	read_byte = 1;
 	while ((ft_strchar(line, '\n') == 0) && read_byte > 0)
 	{
-		read_byte = read(fd, temp, BUFFER_SIZE);
-		if (read_byte == -1 || read_byte == 0)
-			return (ft_free(line));
-		temp[read_byte] = '\0';
-		line = ft_strjoin(line, temp);
+		read_byte = read(fd, buffer, BUFFER_SIZE);
+		if (read_byte == -1)
+			return (NULL);
+		buffer[read_byte] = '\0';
+		line = ft_strjoin(line, buffer);
 		if (!line)
-			return (temp);
+			return (NULL);
 	}
 	if (read_byte == -1)
-		return (ft_free(temp));
-	free(temp);
+		return (NULL);
 	return (line);
 }
 
@@ -86,7 +76,8 @@ char	*get_next_line(int fd)
 	char		*line;
 	char		*temp;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= 2147483647 || read(fd, 0, 0))
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= 2147483647 || read(fd, 0,
+			0) == -1)
 		return (NULL);
 	temp = ft_read_size(fd, next_line);
 	if (!temp)
@@ -94,5 +85,6 @@ char	*get_next_line(int fd)
 	line = ft_read_line(temp);
 	if (temp[ft_strlen(line)] != '\0')
 		next_line = &temp[ft_strlen(line) + 1];
+	free(temp);
 	return (line);
 }
